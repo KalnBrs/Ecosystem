45b89b582694ef2987fc85503a92d8d203ef96e1
Kaelan Brose <kbrose2776@gmail.com>
2026-05-04 11:10:23 -0500

    Changed next/router to next/navigation to follow app router standards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 "use client"
 import { signIn } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-  const isClient = typeof window !== 'undefined';
-
-if (!isClient) {
-  return null;
-} // Prevent rendering during SSR
 
   return (
     <div>
