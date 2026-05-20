68d6b509865d627290011fe2910f3e8f231db6a4
Kaelan Brose <kbrose2776@gmail.com>
2026-05-20 11:18:07 -0500

    Created the post function for the nodes

--- src/app/api/nodes/route.ts
+++ src/app/api/nodes/route.ts
@@ -1,5 +1,6 @@
 import { authOptions } from "@/lib/auth";
-import { listNodes } from "@/services/NodeService";
+import { CreateNodeSchema } from "@/lib/schemas/node.schema";
+import { createNode, listNodes } from "@/services/NodeService";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
@@ -29,6 +30,32 @@ export async function GET(request: Request) {
     if (error instanceof Error) {
       return NextResponse.json(
         {message: 'An error occurred while retriving nodes: ' + error.message},
+        {status: 500}
+      )
+    } else {
+      return NextResponse.json(
+        {message: 'An unexpected error occurred' + error},
+        {status: 500}
+      )
+    }
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const parsed = CreateNodeSchema.parse(request.body);
+
+    const node = await createNode(parsed);
+
+    return NextResponse.json(
+      {message: `Created a node with a title of ${parsed.title}`, data: node},
+      {status: 201}
+    )
+
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {message: 'An error occurred while trying to create a node: ' + error.message},
         {status: 500}
       )
     } else {
