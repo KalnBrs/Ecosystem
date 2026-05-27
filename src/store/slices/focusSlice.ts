717a038e73e7de5f02770ae81985be925c6c489c
Kaelan Brose <kbrose2776@gmail.com>
2026-05-27 09:44:05 -0500

    Created focusSlice

---
+++ src/store/slices/focusSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export type focusMode = "stopwatch" | "timer" | null
+
+export interface focusState {
+  activeTaskId: string | null,
+  mode: focusMode,
+  timerDurationMinutes: number | null,
+  startedAt: string | null,
+  elapsedSeconds: number,
+  isRunning: boolean
+}
+
+const initialState = {
+  activeTaskId: null,
+  mode: null,
+  timerDurationMinutes: null,
+  startedAt: null,
+  elapsedSeconds: 0,
+  isRunning: false
+}
+
+export const focusSlice = createSlice({
+  name: "focus",
+  initialState,
+  reducers: {
+
+  }
+})
